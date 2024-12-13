358d5e4c579f6467f6b03f87e9721180723d83eb
Lily Hopkins <lily@hpkns.uk>
2024-12-13 16:12:06 +0000

    1.2.0-rc.2 (#100)
    * reset version to 1.1.0 after merge from rc branch
    
    * Merge branch 'next' into 1.1.0
    
    * Added .editorconfig
    
    * feat(cli): refactored cli
    
    resolves #84, entirely rewrites the CLI interface, but makes it more stable going forward
    
    * fix(cli): terminate after early commands
    
    * 1.1.0 (#77)
    
    * reset version to 1.1.0 after merge from rc branch
    
    * 1.1.0-rc.2 (#83)
    
    * reset version to 1.1.0 after merge from rc branch
    
    * ci: updated for next branch
    
    resolves #80
    
    * bumped rc version
    
    * remove initial folder
    
    resolves #82
    
    * added extra logging to help diagnose #81
    
    * actually export package description
    
    * reformatted
    
    * Merge remote-tracking branch 'origin/next' into 1.1.0-rc.2
    
    * 1.1.0-rc.3 (#87)
    
    * reset version to 1.1.0 after merge from rc branch
    
    * fix: test case schema referred to wrong schema in $schema json field
    
    * fix: fixed returning wrong error
    
    * fix: interrupt the write when a test case or manifest fails schema validation
    
    fixes #86
    
    * refactor: documented all private items
    
    * chore: bumped version
    
    ---------
    
    Co-authored-by: Lily Hopkins <lily@hpkns.uk>
    
    * Merge branch 'main' into 1.2.0
    
    * chore: bumped version
    
    * feat(ui): display files better in the UI
    
    resolves #90
    
    * refactor(ui): refactored lookup_with_args building arguments
    
    * feat(ui): add file evidence add button to ui
    
    resolves #93
    
    * fix(ui): fix opening metadata triggering a need for save
    
    fixes #88
    
    * style: reformatted
    
    * feat(exporters): better handle files in html export
    
    resolves #91
    
    * feat(exporters): rewrite excel exporting of file data
    
    resolves #92
    
    * feat(exporters): added ZIP of files exporter
    
    resolves #94
    
    * Merge branch 'next' into 1.2.0
    
    * fix(ui): ellipsize file names in evidence
    
    * fix(exporters): check if no files will be exported before exporting zip of files
    
    resolves #99
    
    * fix(exporters): disambiguate duplicate file names on zip of files export
    
    fixes #98
    
    * chore: bumped version
    
    * style: reformatted
    
    * style: applied clippy suggestions

--- src/exporters/zip_of_files.rs
+++ src/exporters/zip_of_files.rs
@@ -1,8 +1,10 @@
 use std::{
+    collections::HashMap,
     fs,
     io::{self, BufWriter, Cursor},
 };
 
+use thiserror::Error;
 use uuid::Uuid;
 use zip::{write::SimpleFileOptions, ZipWriter};
 
@@ -13,6 +15,14 @@ use super::Exporter;
 /// An exporter to an ZIP of the files in the package.
 #[derive(Default)]
 pub struct ZipOfFilesExporter;
+
+/// An error from the [`ZipOfFilesExporter`].
+#[derive(Debug, Error)]
+pub enum ZipOfFilesError {
+    /// No files are in this package, so no output would be produced
+    #[error("no files are in this package, so no output would be produced")]
+    NoFilesToExport,
+}
 
 impl Exporter for ZipOfFilesExporter {
     fn export_name() -> String {
@@ -28,6 +38,19 @@ impl Exporter for ZipOfFilesExporter {
         package: &mut EvidencePackage,
         path: std::path::PathBuf,
     ) -> crate::Result<()> {
+        let mut has_files = false;
+        for test_case in package.test_case_iter()? {
+            if check_has_files(test_case) {
+                has_files = true;
+                break;
+            }
+        }
+        if !has_files {
+            return Err(crate::Error::OtherExportError(Box::new(
+                ZipOfFilesError::NoFilesToExport,
+            )));
+        }
+
         let mut zip = ZipWriter::new(BufWriter::new(
             fs::File::create(path).map_err(|e| crate::Error::OtherExportError(Box::new(e)))?,
         ));
@@ -49,14 +72,21 @@ impl Exporter for ZipOfFilesExporter {
         case: Uuid,
         path: std::path::PathBuf,
     ) -> crate::Result<()> {
-        let mut zip = ZipWriter::new(BufWriter::new(
-            fs::File::create(path).map_err(|e| crate::Error::OtherExportError(Box::new(e)))?,
-        ));
         let case = package
             .test_case(case)?
             .ok_or(crate::Error::OtherExportError(
                 "Test case not found!".into(),
             ))?;
+
+        if !check_has_files(case) {
+            return Err(crate::Error::OtherExportError(Box::new(
+                ZipOfFilesError::NoFilesToExport,
+            )));
+        }
+
+        let mut zip = ZipWriter::new(BufWriter::new(
+            fs::File::create(path).map_err(|e| crate::Error::OtherExportError(Box::new(e)))?,
+        ));
 
         add_test_case_to_zip(&mut zip, package.clone(), case)
             .map_err(crate::Error::OtherExportError)?;
@@ -68,6 +98,16 @@ impl Exporter for ZipOfFilesExporter {
     }
 }
 
+/// Check is this test case contains any file evidence
+fn check_has_files(test_case: &TestCase) -> bool {
+    for ev in test_case.evidence() {
+        if let EvidenceKind::File = ev.kind() {
+            return true;
+        }
+    }
+    false
+}
+
 /// Create the worksheet that holds the test case's information
 fn add_test_case_to_zip(
     zip: &mut ZipWriter<BufWriter<fs::File>>,
@@ -75,6 +115,15 @@ fn add_test_case_to_zip(
     test_case: &TestCase,
 ) -> Result<(), Box<dyn std::error::Error>> {
     log::debug!("Creating ZIP of files for test case {}", test_case.id());
+
+    let mut filename_count = HashMap::new();
+    for ev in test_case.evidence() {
+        if let EvidenceKind::File = ev.kind() {
+            if let Some(filename) = ev.original_filename() {
+                filename_count.insert(filename, filename_count.get(filename).unwrap_or(&0) + 1);
+            }
+        }
+    }
 
     let mut unnamed_counter = 0;
 
@@ -92,9 +141,26 @@ fn add_test_case_to_zip(
                 format!("unnamed-{unnamed_counter}")
             };
 
+            let disambiguator = if let Some(count) = filename_count.get(&name) {
+                if *count > 1 {
+                    if let Some(caption) = evidence.caption() {
+                        format!("({}) ", caption)
+                    } else {
+                        "".to_string()
+                    }
+                } else {
+                    "".to_string()
+                }
+            } else {
+                "".to_string()
+            };
+
             // Add to ZIP file
-            zip.start_file(name, SimpleFileOptions::default())
-                .map_err(|e| crate::Error::OtherExportError(Box::new(e)))?;
+            zip.start_file(
+                format!("{}/{disambiguator}{name}", test_case.metadata().title()),
+                SimpleFileOptions::default(),
+            )
+            .map_err(|e| crate::Error::OtherExportError(Box::new(e)))?;
             let mut data_cursor = Cursor::new(data);
             io::copy(&mut data_cursor, zip)
                 .map_err(|e| crate::Error::OtherExportError(Box::new(e)))?;
