d7d7c5650cd31d2da427029c382a91b3b0d44664
Lily Hopkins <lily@hpkns.uk>
2025-05-02 17:43:32 +0100

    feat: add scroll beyond bottom
    resolves #156

--- src/evidenceangel-ui/app.rs
+++ src/evidenceangel-ui/app.rs
@@ -572,6 +572,7 @@ impl Component for AppModel {
                                                 set_orientation: gtk::Orientation::Vertical,
                                                 set_hexpand: true,
                                                 set_halign: gtk::Align::Fill,
+                                                set_height_request: 350,
                                                 set_spacing: 2,
 
                                                 add_controller = gtk::DropTarget {
