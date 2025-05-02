387c8674a4d77872ea97470585ff2d3635074f3b
Lily Hopkins <lily@hpkns.uk>
2025-05-02 23:14:23 +0100

    build: removed gtk dependency for cli only builds

--- build.rs
+++ build.rs
@@ -1,13 +1,16 @@
 fn main() {
     if cfg!(feature = "cli") || cfg!(feature = "ui") {
-        // Build hicolor icons
-        println!("cargo::rerun-if-changed=resources");
-        println!("cargo::rerun-if-changed=hicolor-icon.gresource.xml");
-        glib_build_tools::compile_resources(
-            &["resources"],
-            "hicolor-icon.gresource.xml",
-            "hicolor-icon.gresource",
-        );
+        #[cfg(feature = "ui")]
+        {
+            // Build hicolor icons
+            println!("cargo::rerun-if-changed=resources");
+            println!("cargo::rerun-if-changed=hicolor-icon.gresource.xml");
+            glib_build_tools::compile_resources(
+                &["resources"],
+                "hicolor-icon.gresource.xml",
+                "hicolor-icon.gresource",
+            );
+        }
 
         // Build documentation
         println!("cargo::rerun-if-changed=docs/book.toml");
