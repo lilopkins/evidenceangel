19e18250fcc416bddd5264aa9729c07e150b8455
Lily Hopkins <lily@hpkns.uk>
2025-05-02 23:46:07 +0100

    1.5.0-alpha.5 (#170)
    * chore: bumped version
    
    * ci: replaced uuid generation

--- wix/generate_wix_script.sh
+++ wix/generate_wix_script.sh
@@ -5,6 +5,36 @@ SOURCE_PATH="$GTK4_PATH"
 TEMPLATE_FILE="main.wxs.in"
 OUTPUT_FILE="main.wxs"
 
+# Modified from: https://github.com/lowerpower/UUID-with-bash
+
+setVariant()
+{
+    # set Variant  RFC 4122  01xx xxxx xxxx xxxx
+	printf "%x" $(((0x$1 & 0x3fff) | 0x8000))
+}
+
+#
+# Generate Compleatly Random UUID based on urandom
+#
+generate_uuid_v4()
+{
+	#first 4 octets (set1)  (XXXXXXXX-0000-0000-0000-000000000000)
+    eight1=$(tr -dc a-f0-9 < /dev/urandom | dd bs=8 count=1 2> /dev/null)
+    #next 2 octets   (00000000-XXXX-0000-0000-000000000000)
+	four2=$(tr -dc a-f0-9 < /dev/urandom | dd bs=4 count=1 2> /dev/null)
+    #next 2 octets less 4msb   (00000000-0000-XXXX-0000-000000000000)
+    #Version  xxxxxxxx-xxxx-Vxxx-xxxx-xxxxxxxxxxxx
+    three3=$(tr -dc a-f0-9 < /dev/urandom | dd bs=3 count=1 2> /dev/null)
+    #next 2 octets less  (00000000-0000-0000-XXXX-000000000000)
+    four4=$(tr -dc a-f0-9 < /dev/urandom | dd bs=4 count=1 2> /dev/null)
+    four4=$(setVariant "$four4")
+    #last 6 octets   (00000000-0000-0000-0000-XXXXXXXXXXXX)
+    twelve5=$(tr -dc a-f0-9 < /dev/urandom | dd bs=12 count=1 2> /dev/null)
+
+    #we prepend the version 4 before the 3 nibbels of the 3rd set
+    printf "${eight1}-${four2}-4${three3}-${four4}-${twelve5}"
+}
+
 # Initialize an empty string for the snippets
 snippets=""
 component_refs=""
@@ -20,7 +50,7 @@ add_directory() {
             local dirname
             dirname="$(basename "$file")"
             local dirid
-            dirid="$(uuidgen)"
+            dirid="$(generate_uuid_v4)"
             snippets+="<Directory Id='_${dirid//-/}' Name='$dirname'>\n"
             # If it's a directory, call the function recursively
             add_directory "$file"
@@ -28,7 +58,7 @@ add_directory() {
         elif [ -f "$file" ]; then
             # If it's a file, generate the component snippet
             local file_id
-            file_id="$(uuidgen)"
+            file_id="$(generate_uuid_v4)"
             local file_name
             file_name="$(basename "$file")"
             snippets+="<Component Id='_${file_id//-/}' Guid='$file_id'>\n"
